a7c4471e2672040799602f4bee852e248f1b563d
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-05-26 15:26:37 +0300

    Done with Lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Mr. Chocolate", 19, "M", "dark brown", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Rowdy", 36, "F", "white", "angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Tabitha", 13, "F", "peach", "calm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Sargeant Brown", 20, "M", "brown", "aggresive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Melissa", 9, "F", "black", "intense", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Grinch", 18, "M", "green", "sad", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Wendy", 20, "F", "dark brown", "fiesty", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES (NULL, 3, "M", "gray", "goofy", 1);
